a96801c60cdf5b6ad03c99e344319666651d6084
ForteScarlet <ForteScarlet@163.com>
2023-12-01 00:51:18 +0800

    Annotation mark

--- simbot-logger/src/commonMain/kotlin/love/forte/simbot/logger/LoggerFactory.kt
+++ simbot-logger/src/commonMain/kotlin/love/forte/simbot/logger/LoggerFactory.kt
@@ -12,20 +12,17 @@
 
 package love.forte.simbot.logger
 
-import kotlin.jvm.JvmStatic
-
 /**
  * 日志工厂。
  *
  */
 public expect object LoggerFactory {
-    
+
     /**
      * 根据名称获取一个 [Logger] 实例。
      */
-    @JvmStatic
     public fun getLogger(name: String): Logger
-    
+
 }
 
 /**
